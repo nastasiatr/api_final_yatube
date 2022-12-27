46cd093b29760e1e7fd8b5f77d22c61ade24e5d1
Анастасия Трусова <trusovaanastasia@mail.ru>
2022-12-27 14:52:25 +0800

    2 варинат. 9 спринт

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import AuthorPermission
